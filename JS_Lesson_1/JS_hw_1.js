ddea91e83f45741b26d8613d711e560f28daa508
Artemius Morningstar <artemius.morningstar@gmail.com>
2023-11-22 10:51:29 +0200

    HW_1 Done

---
+++ JS_Lesson_1/JS_hw_1.js
@@ -0,0 +1,27 @@
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua'
+let one = 1;
+let ten = 10;
+let minus = -999;
+let ott = 123;
+let pi = 3.14;
+let frac = 2.7;
+let age = 16;
+let t = true;
+let f = false;
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(minus);
+console.log(ott);
+console.log(pi);
+console.log(frac);
+console.log(age);
+console.log(t);
+console.log(f);
