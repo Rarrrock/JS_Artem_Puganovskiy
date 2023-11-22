55179cebab10b4eb52b8cd6d1ba8459c31688ddc
Artemius Morningstar <artemius.morningstar@gmail.com>
2023-11-22 11:14:18 +0200

    HW_4_dop Done

---
+++ JS_Lesson_1/JS_hw_4_dop.js
@@ -0,0 +1,8 @@
+let name = prompt('Як тебе звуть?');
+let family = prompt('Твоє прізвище?');
+let newAge = prompt('Скільки тобі років?');
+
+
+console.log(name);
+console.log(family);
+console.log(newAge);
