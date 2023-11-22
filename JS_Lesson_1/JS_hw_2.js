9d4bc2940a9d9f54a7a684a5d57d50d2777edaa1
Artemius Morningstar <artemius.morningstar@gmail.com>
2023-11-22 18:18:57 +0200

    HW_4_dop Done

--- JS_Lesson_1/JS_hw_2.js
+++ JS_Lesson_1/JS_hw_2.js
@@ -1,3 +1,7 @@
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+//      З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+
 let firstName = 'Артём';
 let middleName = 'Олегович';
 let lastName = 'Пугановский';
