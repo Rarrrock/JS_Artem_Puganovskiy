90aac3ce8231dfefeca83762dbfaecb8ae33c7ae
Artemius Morningstar <artemius.morningstar@gmail.com>
2023-11-23 16:26:58 +0200

    Les_2_HW_1   Done

---
+++ JS_Lesson_2/JS_Les_2_hw_1.js
@@ -0,0 +1,132 @@
+<!--Масиви та об'єкти:-->
+<!-- - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль-->
+<!-- - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.-->
+<!-- - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.-->
+<!-- - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача-->
+
+
+<!-- - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль-->
+
+let a, b, c;
+
+let bbbbbb = {
+    id: 1,
+    name: 'dfhfdb',
+    age: 567
+}
+
+let arr1 = ['sfbbs', 'ilkk', 217, '8959', 'AAAA', a, b, c, bbbbbb["id"], bbbbbb.age];
+
+console.log(arr1);
+
+
+<!-- - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.-->
+
+let book1={
+    title: 'AAAAAAAAA',
+    pageCount: 623,
+    genre: 'ooo',
+}
+
+let book2={
+    title: 'BBBBBBB',
+    pageCount: 111,
+    genre: 'hhhh',
+}
+
+let book3={
+    title: 'CCCCCCC',
+    pageCount: 35,
+    genre: 'mmbb',
+}
+
+
+<!-- - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.-->
+
+let book4={
+    title: 'AAAAAAAAA',
+    pageCount: 623,
+    genre: 'ooo',
+    authors: ['AAA', 43]
+}
+
+let book5={
+    title: 'BBBBBBB',
+    pageCount: 111,
+    genre: 'hhhh',
+    authors: ['BBB', 24]
+}
+
+let book6={
+    title: 'CCCCCCC',
+    pageCount: 35,
+    genre: 'mmbb',
+    authors: ['CCC', 11]
+}
+
+
+<!-- - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача-->
+
+let arr10ob = [
+    user0 = {
+        name: 'Aaaaa',
+        username: 'wegkjbew',
+        password: '1786514',
+    },
+    user1 = {
+        name: 'Bbbbbb',
+        username: 'nrtjtfjft',
+        password: '239865792836',
+    },
+    user2 = {
+        name: 'Ccccc',
+        username: 's;dkbnsdnhjl',
+        password: '315976',
+    },
+    user3 = {
+        name: 'Dddddd',
+        username: 'luwedhguwd',
+        password: '123',
+    },
+    user4 = {
+        name: 'Eeeee',
+        username: 'slugbuhdsl',
+        password: '066',
+    },
+    user5 = {
+        name: 'Ffffff',
+        username: 'ldsivnlsn',
+        password: '277423',
+    },
+    user6 = {
+        name: 'Ggggg',
+        username: 'kmlkjbh',
+        password: '00886',
+    },
+    user7 = {
+        name: 'Hhhhh',
+        username: 'lkajxncvgvy',
+        password: '235322',
+    },
+    user8 = {
+        name: 'Iiiiii',
+        username: 'lubagsfc',
+        password: '662954',
+    },
+    user9 = {
+        name: 'Jjjjj',
+        username: 'adsgflgjbhweg',
+        password: '8545682',
+    }
+]
+
+console.log(arr10ob[0].password);
+console.log(arr10ob[1].password);
+console.log(arr10ob[2].password);
+console.log(arr10ob[3].password);
+console.log(arr10ob[4].password);
+console.log(arr10ob[5].password);
+console.log(arr10ob[6].password);
+console.log(arr10ob[7].password);
+console.log(arr10ob[8].password);
+console.log(arr10ob[9].password);
