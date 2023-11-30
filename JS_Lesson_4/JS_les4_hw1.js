00c0783355ed16f769dc0e8eb4127e3662c0c3a0
Artemius Morningstar <artemius.morningstar@gmail.com>
2023-11-30 16:55:08 +0200

    Les_4_HW_1 Done

--- JS_Lesson_4/JS_les4_hw1.js
+++ JS_Lesson_4/JS_les4_hw1.js
@@ -3,3 +3,52 @@
 <!-- - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r-->
 <!-- - створити функцію яка приймає масив та виводить кожен його елемент-->
 
+
+<!-- - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б-->
+let a = 256;
+let b = 89;
+function areaRectangle (sideA, sideB) {
+    return sideA * sideB;
+}
+console.log('Площа Прямокутника = ', areaRectangle(a,b));
+
+
+<!-- - створити функцію яка обчислює та повертає площу кола з радіусом r-->
+let r = 12;
+function areaRound (radius) {
+    return Math.PI * radius * radius;
+}
+console.log('Площа Кола = ', areaRound(r));
+
+
+<!-- - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r-->
+
+let h = 18;
+function areaCylinder (radius, height) {
+    return (2 * Math.PI * radius * (height + radius));
+}
+console.log('Площа Циліндра = ', areaCylinder(r,h));
+
+
+<!-- - створити функцію яка приймає масив та виводить кожен його елемент-->
+
+let arr = [1,4,7,234,8,1]
+function output (a) {
+    for (let i = 0; i < a.length; i++) {
+        console.log(a[i]);
+    }
+}
+output(arr);
+
+
+
+
+
+
+
+// function areaRectangle (sideA, sideB) {
+//     document.write('<li>${sideA * sideB}</li>')
+//     return sideA * sideB;
+// }
+//
+// areaRectangle(a,b);
