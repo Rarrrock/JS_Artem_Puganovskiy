db495285678bcfed62a7f677e8177df30685e48a
Artemius Morningstar <artemius.morningstar@gmail.com>
2023-12-16 15:56:07 +0200

    Les_8_HW_4 DONE

--- JS_lesson_8/JS_les8_hw_4.js
+++ JS_lesson_8/JS_les8_hw_4.js
@@ -1,24 +1,26 @@
-<!--    'JS_les8_hw_3.js'-->
-<!-- - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:-->
-
+<!-- - (Те саме, тільки через клас)-->
+<!--Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:-->
 <!--&#45;&#45; drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`-->
 <!--&#45;&#45; info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`-->
 <!--&#45;&#45; increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed-->
 <!--&#45;&#45; changeYear (newValue) - змінює рік випуску на значення newValue-->
 <!--&#45;&#45; addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car-->
 
-function Car (model, producer, year, maxSpeed, volume) {
-    this.model = model;
-    this.producer = producer;
-    this.year = year;
-    this.maxSpeed = maxSpeed;
-    this.volume = volume;
 
-    this.drive = function () {
+class Car2 {
+    constructor(model, producer, year, maxSpeed, volume){
+        this.model = model;
+        this.producer = producer;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.volume = volume;
+    }
+
+    drive() {
         console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`);
     }
 
-    this.info = function() {
+    info() {
         console.log('model : ', this.model);
         console.log('producer : ', this.producer);
         console.log('year : ', this.year);
@@ -26,24 +28,22 @@ function Car (model, producer, year, maxSpeed, volume) {
         console.log('volume : ', this.volume);
     }
 
-    this.increaseMaxSpeed = function(newSpeed) {
+    increaseMaxSpeed (newSpeed) {
         this.maxSpeed += newSpeed;
     }
 
-    this.changeYear = function (newYear) {
+    changeYear (newYear) {
         this.year = newYear;
     }
 
-    this.addDriver = function (driver) {
+    addDriver (driver) {
         this.driver = driver;
     }
 }
 
-const car1 = new Car('ferrari','manufacturer',1998,360,50);
-car1.drive();
-car1.info();
-car1.increaseMaxSpeed(51);
-car1.changeYear(2001);
-car1.addDriver({name:'Parampam', age:31});
-
-
+const car2 = new Car2('Mitsu','producer',2011,270,40);
+car2.drive();
+car2.info();
+car2.increaseMaxSpeed(21);
+car2.changeYear(2015);
+car2.addDriver({name:'Palala', age:21});
